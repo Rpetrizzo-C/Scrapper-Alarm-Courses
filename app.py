a86978288b63eeedee5f0e3ff7c7f6585353c3f5
Rodrigo Petrizzo <petrizzo.rodrigoa@gmail.com>
2021-01-20 04:57:07 -0300

    Revert "Commit Change to OOP"
    This reverts commit ac774e704ab4f3468537f3a6aba5230e8aa4468a.

--- app.py
+++ app.py
@@ -4,18 +4,19 @@ url = "https://allcoursefree.com/"
 
 headers = {"User-Agent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.141 Safari/537.36'}
 
-class content:
-    def check_status(self):
-        page = requests.get(url, headers=headers)
 
-        soup = BeautifulSoup(page.content, 'html.parser')
+def check_status():
+    page = requests.get(url, headers=headers)
 
-        title = soup.find('h1', attrs={"class":'site-title'}).get_text()
-        courses = soup.find('p', attrs={"class":'woocommerce-result-count'}).get_text()
-         
-        quantity = int(courses[-11:-8])
-        
-page_content = content()       
-page_content.check_status()
+    soup = BeautifulSoup(page.content, 'html.parser')
+
+    title = soup.find('h1', attrs={"class":'site-title'}).get_text()
+    courses = soup.find('p', attrs={"class":'woocommerce-result-count'}).get_text()
+    print(title) 
+    quantity = int(courses[-11:-8])
+    print (quantity)
+    if quantity != 13:
+        print("idiot")
+check_status()
 
   
